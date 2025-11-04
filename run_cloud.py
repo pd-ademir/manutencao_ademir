e170b0f52d8f7ddbdcecf30ec893dfa59c062656
ornilio <orniliooficial@outlook.com>
2025-11-04 15:03:34 -0300

    first commit3

--- run_cloud.py
+++ run_cloud.py
@@ -7,4 +7,6 @@ os.environ['FLASK_ENV'] = 'production'
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    #app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
